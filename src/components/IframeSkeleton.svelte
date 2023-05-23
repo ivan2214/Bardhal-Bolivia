39545d519ad37e37971bf7348a4a7142f38de0d5
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-05-22 23:50:23 -0300

    add more information

---
+++ src/components/IframeSkeleton.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let src, className, width, height, alt, id
+  let cargada = false
+
+  $: {
+    const iframe = document?.createElement('iframe')
+    iframe.src = src
+    iframe.onload = () => {
+      cargada = true
+    }
+  }
+  console.log(cargada)
+</script>
+
+{#if cargada}
+  <div
+    role="status"
+    class={`max-w-sm w-full p-2 border border-gray-200 rounded shadow animate-pulse ${className} dark:border-gray-300`}
+  >
+    <img
+      id={id || ''}
+      width={width || '100'}
+      height={height || '100'}
+      src="loadedImage.svg"
+      class={` mx-auto`}
+      alt={alt || ''}
+    />
+
+    <span class="sr-only">Loading...</span>
+  </div>
+{:else}
+  <iframe
+    id={id || ''}
+    loading="lazy"
+    class={`object-cover mx-auto w-full rounded-lg`}
+    width={width || '100'}
+    height={height || '100'}
+    {src}
+    title={alt || 'iframe description here'}
+    alt={alt || ''}
+  />
+{/if}
